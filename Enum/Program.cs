8e0eee0dd12ca6e36d9f776fb7b6a720f54a92f0
Aleksandr22822 <alek5andr.bubnov@yandex.ru>
2024-03-03 17:39:43 +0300

    Добавьте файлы проекта.

---
+++ Enum/Program.cs
@@ -0,0 +1,71 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Symbol result = Symbol.e;
+      
+  
+        int b = 4;
+        int R = (int)(b + result); 
+       // Console.WriteLine(R);   
+
+        DaysOfWeek day = DaysOfWeek.Monday;
+        if (day == DaysOfWeek.Monday) 
+        { 
+        }
+        // Console.WriteLine("Началась рабочая неделя!");
+
+        DayTime now = DayTime.Evening;
+        void PrintMessage(DayTime dayTime)
+        {
+            switch (dayTime)
+            {
+                case DayTime.Morning:
+                    Console.WriteLine("Доброе утро");
+                    break;
+                case DayTime.Afternoon:
+                    Console.WriteLine("Добрый день");
+                    break;
+                case DayTime.Evening:
+                    Console.WriteLine("Добрый вечер");
+                    break;
+                case DayTime.Night:
+                    Console.WriteLine("Доброй ночи");
+                    break;
+            }
+        }
+
+        PrintMessage(now);                   // Добрый вечер
+        PrintMessage(DayTime.Afternoon);    // Добрый день
+        PrintMessage(DayTime.Night);
+    }
+
+    enum Symbol
+    { 
+        a = 6,
+        b,
+        c,
+        d,
+        e
+     }
+
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,   
+        Wednesday, 
+        Friday    
+    }
+
+
+
+   
+    enum DayTime
+    {
+        Morning,
+        Afternoon,
+        Evening,
+        Night
+    }
+}
